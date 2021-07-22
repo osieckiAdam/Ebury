4746700b9e3a306766455c43b9d4db513c56f236
Adam Osiecki <osiecki.adam@outlook.com>
2021-07-22 18:36:49 +0200

    script to prepare test scratch org

--- scripts/cicd/createSO.sh
+++ scripts/cicd/createSO.sh
@@ -24,7 +24,7 @@ then
 fi
 
 #creating Scratch org
-echo "Your scratch org building. Please wait, it could take up to few minutes"
+echo "Your scratch org is building. Please wait, it could take up to few minutes"
 sfdx force:org:create -f config/project-scratch-def.json -a ${ORG_NAME} --json
 
 if [ "$?" = "1" ] 
@@ -33,6 +33,21 @@ then
 	exit 1
 fi
 echo "Your Scratch org is created."
+
+#import chatter group
+RES=$(sfdx force:data:tree:import --json -f scripts/cicd/CollaborationGroup.json -u ${ORG_NAME})
+
+#assign group id to variable
+GROUPID=$(echo ${RES} | grep -o '"[^"]*"\s*:\s*"[^"]*"' | grep -E '^"(id)"' | cut -b 8-25)
+echo ${GROUPID}
+
+#make backup copy of original Trade Insert process builder, we need to do that in order to change Id on the pb
+mkdir -p temp
+cp force-app/main/default/flows/Trade_Insert.flow-meta.xml temp
+
+#replace original Id of the group to new group created on scratch org
+sed -i "s/0F93X000000ghqLSAQ/${GROUPID}/" force-app/main/default/flows/Trade_Insert.flow-meta.xml
+sed -i "s/0F93X000000ghqLSAQ/${GROUPID}/" force-app/main/default/flows/Trade_Insert.flow-meta.xml
 
 #deploying metadata:
 echo "Metadata deploy started"
@@ -45,6 +60,11 @@ then
 fi
 
 echo "Metadata was deployed succesfully."
+
+#bring back original process builder
+rm force-app/main/default/flows/Trade_Insert.flow-meta.xml
+cp temp/Trade_Insert.flow-meta.xml force-app/main/default/flows
+rm -rf temp
 
 sfdx force:user:permset:assign -n Exchange_Rates_App_User -u ${ORG_NAME} --json
 if [ "$?" = "1" ]
